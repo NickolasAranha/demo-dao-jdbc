58bf0658c525e250a2a2df5dfe904a3caf934365
unknown <aranha.nickolas@gmail.com>
2026-06-26 14:55:03 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -14,6 +14,7 @@ public class Program {
         System.out.println("==== TEST 1: seller findById ====");
         Seller seller = sellerDao.findById(3);
         System.out.println(seller);
+
         System.out.println("\n==== TEST 2: seller findByDepartment ====");
         Department department = new Department(2, null);
         List<Seller> list = sellerDao.findByDepartment(department);
@@ -31,5 +32,11 @@ public class Program {
         Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
         sellerDao.insert(newSeller);
         System.out.println("Inserted! New id = " + newSeller.getId());
+
+        System.out.println("\n==== TEST 5: seller update ====");
+        seller = sellerDao.findById(1);
+        seller.setName("Marta Wayne");
+        sellerDao.update(seller);
+        System.out.println("Updated complete");
     }
 }
